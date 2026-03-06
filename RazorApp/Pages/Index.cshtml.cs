df3a10365fa9f1279e54ddeef1aa37fa6293ca1b
Seluleko Bane <SelulekoB@hollywoodbets.net>
2026-03-06 16:22:48 +0200

    image and index changes

--- RazorApp/Pages/Index.cshtml.cs
+++ RazorApp/Pages/Index.cshtml.cs
@@ -1,20 +1,27 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorApp.Models;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace RazorApp.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly AppDbContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public List<Product> Products { get; set; } = new List<Product>();
+
+        public IndexModel(ILogger<IndexModel> logger, AppDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public void OnGet()
         {
-
+            Products = _context.Product.ToList();
         }
     }
 }
